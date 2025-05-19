01239c37747cdb9e3643d657dfa9c5a0672f9776
DiegoBustos16 <diegombustos16@gmail.com>
2025-05-19 15:46:00 -0300

    test: add AuthGuard, AuthInterceptor and AuthService unit tests

--- src/app/auth/interceptors/auth.interceptor.spec.ts
+++ src/app/auth/interceptors/auth.interceptor.spec.ts
@@ -1,15 +1,43 @@
-import { TestBed } from '@angular/core/testing';
-
 import { AuthInterceptor } from './auth.interceptor';
+import { HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import { of } from 'rxjs';
 
-describe('authInterceptor', () => {
-  let authInterceptor: AuthInterceptor;
+describe('AuthInterceptor (unit test)', () => {
+  let interceptor: AuthInterceptor;
+
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-        authInterceptor = TestBed.inject(AuthInterceptor);
+    interceptor = new AuthInterceptor();
   });
 
-  it('should be created', () => {
-    expect(authInterceptor).toBeTruthy();
+  it('should add Authorization header when token present', (done) => {
+    spyOn(localStorage, 'getItem').and.returnValue('my-token');
+
+    const req = new HttpRequest('GET', '/path');
+
+    const next: HttpHandler = {
+      handle: (r: HttpRequest<any>) => {
+        expect(r.headers.has('Authorization')).toBeTrue();
+        expect(r.headers.get('Authorization')).toBe('Bearer my-token');
+        return of({} as HttpEvent<any>);
+      }
+    };
+
+    interceptor.intercept(req, next).subscribe({
+      complete: () => done()
+    });
+  });
+
+  it('should not add Authorization header when no token', (done) => {
+    spyOn(localStorage, 'getItem').and.returnValue(null);
+    const req = new HttpRequest("GET", '/any');
+    const next: HttpHandler = {
+      handle: (r: HttpRequest<any>) => {
+        expect(r.headers.has('Authorization')).toBeFalse();
+        return of({} as HttpEvent<any>);
+      }
+    };
+    interceptor.intercept(req, next).subscribe({
+      complete: () => done()
+    });
   });
 });
