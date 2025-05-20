0d2ff2a6380f25bdd081889e6ed7b6f3c8562555
DiegoBustos16 <diegombustos16@gmail.com>
2025-05-20 14:45:38 -0300

    fix: update routerLink in login component and include RouterLink in imports

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, NgForm } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../auth/services/auth.service';
 import { LoginRequest } from '../../models/login-request.model';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
