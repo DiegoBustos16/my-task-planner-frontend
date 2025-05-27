cd30e3a86850d4b9aaf31d9b67a1992fac5e7141
DiegoBustos16 <diegombustos16@gmail.com>
2025-05-27 19:57:13 -0300

    fix: fixed misconfigured interceptor not attaching Authorization header

--- src/app/auth/interceptors/auth.interceptor.ts
+++ src/app/auth/interceptors/auth.interceptor.ts
@@ -1,11 +1,18 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject, PLATFORM_ID } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import { isPlatformBrowser } from '@angular/common';
 import { Observable } from 'rxjs';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const token = localStorage.getItem('authToken');
+    let token: string | null = null;
+
+    if (isPlatformBrowser(this.platformId)) {
+      token = localStorage.getItem('authToken');
+    }
 
     if (token) {
       const cloned = req.clone({
