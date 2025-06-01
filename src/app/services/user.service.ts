caa6912615aa4256178eca5bf6ffbfdfe8504c33
DiegoBustos16 <diegombustos16@gmail.com>
2025-06-01 17:33:20 -0300

    feat: implement user service methods for updating user details and password management

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,7 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { API_ENDPOINTS } from '../constants/api-endpoints';
-import { User } from '../models/user.model'; // Assuming you have a User model defined
+import { User } from '../models/user.model';
+import { UserUpdate } from '../models/user-update.model';
+import { PasswordUpdate } from '../models/password-update.model';
 
 @Injectable({
   providedIn: 'root'
@@ -13,5 +15,17 @@ export class UserService {
 
   getUser(): Observable<User> {
     return this.http.get<User>(API_ENDPOINTS.users.getCurrent);
+  }
+
+  updateUser(data: UserUpdate): Observable<User> {
+    return this.http.patch<User>(API_ENDPOINTS.users.update, data);
+  }
+
+  updatePassword(data: PasswordUpdate): Observable<void> {
+    return this.http.patch<void>(API_ENDPOINTS.users.updatePassword, data);
+  }
+
+  deleteUser(): Observable<void> {
+    return this.http.delete<void>(`${API_ENDPOINTS.users.delete}`);
   }
 }
